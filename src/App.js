32893aaaacd0402b29c582eaa6aa495d62379f3a
Hugo KELHETTER <hkelhetter@unistra.fr>
2022-02-25 15:03:23 +0100

    il est possible de créer un itinéraire à partir de plusieurs adresses

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./style/map.css";
 import CustomMarker from "./compoonents/CustomMarker";
 
 function App() {
+  const [location, setLocation] = useState("plobsheim");
   const MAPBOX_TOKEN =
     "pk.eyJ1IjoiamJoYXJpIiwiYSI6ImNreXlmeWZsYzBqczEydnFrZjZoeDJqMmEifQ.7Z9vGxLMr0cWskUyVAZXZQ";
   const [display, setdisplay] = useState(false);
@@ -16,6 +17,13 @@ function App() {
   return (
     <>
       <button onClick={displayMap}>display map</button>
+      <button onClick={generateRoute}>get itinerary</button>
+      <input
+        type="text"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button onClick={() => searchPlace(location)}>search</button>
       <input type="checkbox" />
       <div id="map"></div>
     </>
