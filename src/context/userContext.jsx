740aa5479eca95ca293b824f1554a6ebabbd1ec4
hkelhetter <hugo.kelhetter@etu.unistra.fr>
2022-06-02 16:55:29 +0200

    test

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -19,9 +19,8 @@ export function UserProvider({ children }) {
         setUser(newUser);
     }
     const [token, setToken] = useState(window.localStorage.getItem("token"))
-    const { isLoading } = useQuery(["whoami", token], async () => {
+    const { isLoading } = useQuery("whoami", async () => {
         if (token != "" && token != null) {
-            console.log("here")
             const res = await refresh(window.localStorage.getItem("refresh_token"))
             updateToken({ setToken, token: res.token, refresh_token: res.refresh_token })
             const whoami = await whoAmI(token)
