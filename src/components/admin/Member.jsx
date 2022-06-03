8d18bdbb434493a75ac07198d770eead2857d774
Gautier <gautier.edel66@gmail.com>
2022-06-03 13:57:06 +0200

    [FIX] routes adduser / removeuser

--- src/components/admin/Member.jsx
+++ src/components/admin/Member.jsx
@@ -1,32 +1,35 @@
 import { useState } from "react";
-import { deleteUser, fetchAllUser, removeUser } from "../../apiCaller";
-import { useQuery, useMutation, useQueryClient } from "react-query";
+import { removeUser } from "../../apiCaller";
+import { useMutation, useQueryClient } from "react-query";
 import { useToken } from "../../context/userContext";
 export default function Member({ member, id, refetchMembers }) {
-    const queryClient = useQueryClient()
-    const [role, setRole] = useState(member.role);
-    const [token] = useToken()
+  const queryClient = useQueryClient();
+  const [role, setRole] = useState(member.role);
+  const [token] = useToken();
 
-    const mutationRemoveUser = useMutation(removeUser, {
-        onSuccess: () => refetchMembers()
-
-    })
-    const handleRoleChange = (e) => {
-        if (e.target.value !== role) {
-            setRole(e.target.value);
-        }
-    };
-    const handleClick = async (email) => {
-        mutationRemoveUser.mutate({ token, email, id })
+  const mutationRemoveUser = useMutation(removeUser, {
+    onSuccess: () => refetchMembers(),
+  });
+  const handleRoleChange = (e) => {
+    if (e.target.value !== role) {
+      setRole(e.target.value);
     }
-    return <li key={member.user.name}>
-        {member.user.firstName} {member.user.lastName}
-        {member.user.tripUsers && member.user.tripUsers[0].role}
-        {/*  <select value={role} onChange={handleRoleChange} className="list-role">
+  };
+  const handleClick = async (email) => {
+    mutationRemoveUser.mutate({ token, email, id });
+  };
+  return (
+    <li key={member.user.name}>
+      {member.user.firstName} {member.user.lastName}
+      {member.user.tripUsers && member.user.tripUsers[0].role}
+      {/*  <select value={role} onChange={handleRoleChange} className="list-role">
             <option value="editor">Editeur</option>
             <option value="guest">Invité</option>
         </select> */}
-        <button className="delete" onClick={() => handleClick(member.user.email)}>Supprimer</button>
-        <hr className="bar" />
+      <button className="delete" onClick={() => handleClick(member.user.email)}>
+        Supprimer
+      </button>
+      <hr className="bar" />
     </li>
-} 
+  );
+}
