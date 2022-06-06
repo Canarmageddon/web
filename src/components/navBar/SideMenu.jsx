cef5630d99aa59d239e04924f79078f49e655a82
Gautier <gautier.edel66@gmail.com>
2022-06-06 14:36:14 +0200

    Bouton déconnexion liste voyages + css

--- src/components/navBar/SideMenu.jsx
+++ src/components/navBar/SideMenu.jsx
@@ -3,9 +3,17 @@ import "../../style/nav.css";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
+import { useUser } from "../../context/userContext";
+import Button from "react-bootstrap/Button";
 
 const SideMenu = ({ setContentPage, showMenu }) => {
+  const [user, setUser] = useUser();
   const navigate = useNavigate();
+
+  const logout = () => {
+    window.localStorage.clear();
+    setUser(undefined);
+  };
 
   return (
     <div className="sidenav" style={{ width: showMenu ? 200 : 0 }}>
@@ -23,6 +31,13 @@ const SideMenu = ({ setContentPage, showMenu }) => {
       <a onClick={() => setContentPage("toDoLists")}>Listes de tâches</a>
       <a onClick={() => setContentPage("admin")}>Administration</a>
       <a onClick={() => setContentPage("details")}>Details</a>
+      <Button
+        variant="danger"
+        onClick={logout}
+        style={{ position: "absolute", bottom: 5, left: 38 }}
+      >
+        Déconnexion
+      </Button>
     </div>
   );
 };
