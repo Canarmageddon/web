bfc3e6dd1c94390fb266528f16e93feb675a5c6e
Gautier <gautier.edel66@gmail.com>
2022-06-20 22:52:11 +0200

    Warning détails poi/step si aucun

--- src/album/Home.jsx
+++ src/album/Home.jsx
@@ -3,14 +3,27 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { getAlbum } from "../apiCaller";
 export default function () {
-    const navigate = useNavigate()
-    const { id, link } = useParams()
-    const { isLoading, isError, data } = useQuery(["album", id], () => getAlbum(id))
-    console.log(data)
-    return <>
-        {!isError && !isLoading &&
-            <>
-                <button onClick={() => navigate(`/unregistered/${id}/${link}/map`)}>Afficher la carte</button>
-                <button onClick={() => navigate(`/unregistered/${id}/${link}/album/${data.id}`)}>Afficher l'album</button>
-            </>}</>
+  const navigate = useNavigate();
+  const { id, link } = useParams();
+  const { isLoading, isError, data } = useQuery(["album", id], () =>
+    getAlbum(id)
+  );
+  return (
+    <>
+      {!isError && !isLoading && (
+        <>
+          <button onClick={() => navigate(`/unregistered/${id}/${link}/map`)}>
+            Afficher la carte
+          </button>
+          <button
+            onClick={() =>
+              navigate(`/unregistered/${id}/${link}/album/${data.id}`)
+            }
+          >
+            Afficher l'album
+          </button>
+        </>
+      )}
+    </>
+  );
 }
